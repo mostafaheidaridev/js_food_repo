da1b94115246b2023ce815f5f793f320098e0851
Mostafa <mostafa_haidare@yahoo.com>
2025-02-06 11:04:30 +0100

    js

---
+++ js/contact.js
@@ -0,0 +1,22 @@
+
+document.querySelector('#frmContact').addEventListener('submit', (e) => {
+
+    e.preventDefault();
+    
+    const name = e.target.txtName.value;
+    const email = e.target.txtEmail.value;
+    const comments = e.target.txtComments.value;
+
+    const submittedInfo = `thank you for your message and we will contact you letter
+
+    Registered Information
+    - name ${name}
+    - email ${email}
+    - comments${comments}
+    `;
+
+    alert(submittedInfo);
+
+    window.location.replace('index.html')
+
+});
